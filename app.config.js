416ec0fd2eb54b5432ba0e5d46aa78e2213f63d9
Cristian Figueredo <cristian28figueredo@gmail.com>
2026-04-03 05:58:46 -0300

    refactor: convert app.config to js so we don't need ts-node

---
+++ app.config.js
@@ -0,0 +1,23 @@
+/**
+ * @param {{ config: import("@expo/config").ExpoConfig }} context
+ * @returns {Partial<import("@expo/config").ExpoConfig>}
+ */
+module.exports = ({ config }) => {
+  const existingPlugins = config.plugins ?? []
+
+  return {
+    ...config,
+    extra: {
+      eas: {
+        projectId: "da879a06-53cf-4a7c-9ebe-41018a162110",
+      },
+    },
+    updates: {
+      url: "https://u.expo.dev/da879a06-53cf-4a7c-9ebe-41018a162110",
+    },
+    runtimeVersion: {
+      policy: "appVersion",
+    },
+    plugins: [...existingPlugins, require("./plugins/withSplashScreen").withSplashScreen],
+  }
+}
